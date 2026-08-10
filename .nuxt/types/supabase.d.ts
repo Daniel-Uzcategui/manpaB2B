3a000d5cee605a311945f3ec119bc419bd99a757
Daniel Uzcategui <44720483+Daniel-Uzcategui@users.noreply.github.com>
2026-08-10 21:15:19 +0200

    chore: add gitignore and untrack node_modules and .nuxt

--- .nuxt/types/supabase.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#supabase/server' {
-  const serverSupabaseClient: typeof import('/home/daniel/Documents/GitHub/manpaB2B/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
-  const serverSupabaseServiceRole: typeof import('/home/daniel/Documents/GitHub/manpaB2B/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
-  const serverSupabaseUser: typeof import('/home/daniel/Documents/GitHub/manpaB2B/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
-  const serverSupabaseSession: typeof import('/home/daniel/Documents/GitHub/manpaB2B/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseSession
-}
